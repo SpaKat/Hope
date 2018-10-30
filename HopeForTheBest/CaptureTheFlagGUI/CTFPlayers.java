2a8a31315d02d30e1b621cfa9aedc129e203a5f2
SpaKat <deanlaga@yahoo.com>
2018-10-29 23:55:13 -0400

    patching exceptions cause by the server and/or client communication.

--- HopeForTheBest/CaptureTheFlagGUI/CTFPlayers.java
+++ HopeForTheBest/CaptureTheFlagGUI/CTFPlayers.java
@@ -8,7 +8,7 @@ import javafx.scene.shape.Circle;
 
 public class CTFPlayers extends Circle{
 
-	
+
 	private Player player;
 	private CTFGame ctfGame;
 	private ArrayList<CTFBullet> bullets;
@@ -39,21 +39,29 @@ public class CTFPlayers extends Circle{
 		if (player.isDisconnect()) {
 			ctfGame.getChildren().remove(this);
 		}
-		for (int i = 0; i < player.getBullets().size(); i++) {
-			Bullet b = player.getBullets().get(i);
-			try {
-				CTFBullet ctfB = bullets.get(i);
-				if (ctfB.done()) {
-					bullets.remove(ctfB);
-			//		bullets.add(new CTFBullet(b,ctfGame));
+		try {
+			for (int i = 0; i < player.getBullets().size(); i++) {
+				Bullet b = player.getBullets().get(i);
+				try {
+					CTFBullet ctfB = bullets.get(i);
+					if (ctfB.done()) {
+						bullets.remove(ctfB);
+						//		bullets.add(new CTFBullet(b,ctfGame));
+					}
+				}catch (Exception e) {
+					bullets.add(new CTFBullet(b,ctfGame));
 				}
-			}catch (Exception e) {
-				bullets.add(new CTFBullet(b,ctfGame));
+
 			}
-			
+		}catch (Exception e) {
+			// TODO: handle exception
 		}
-		for (int i = 0; i < bullets.size(); i++) {
-			bullets.get(i).update();
+		try {
+			for (int i = 0; i < bullets.size(); i++) {
+				bullets.get(i).update();
+			}
+		}catch (Exception e) {
+			// TODO: handle exception
 		}
 		//System.out.println("CTFPlayers: " + player.getX() + "    " + player.getY());
 	}
